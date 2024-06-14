d0b22b1d8a1536f94c1be8f7ca3902f8f4dd312e
Renzo Scuadroni <rdsz1994@hotmail.com>
2024-06-14 19:18:23 -0300

    Update schema and trip procedures

--- src/server/api/routers/trips.ts
+++ src/server/api/routers/trips.ts
@@ -1,11 +1,41 @@
 import { TRPCError } from "@trpc/server";
 import { and, eq, isNull, or } from "drizzle-orm";
+import type { VercelPgDatabase } from "drizzle-orm/vercel-postgres";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { drivers, riders, trips } from "~/server/db/schema";
+import { canceledPendingTrips, drivers, riders, trips } from "~/server/db/schema";
+import type * as schema from "~/server/db/schema";
 
 const TRIP_FIXED_COST = 10;
 const TRIP_METER_COST = 0.0015
+
+const getDriverForTrip = async (db: VercelPgDatabase<typeof schema>, tripId: number | null) => {
+  let availableDrivers = await db.select({ id: drivers.id })
+    .from(drivers)
+    .leftJoin(trips, and(eq(drivers.id, trips.driverId), or(eq(trips.status, 'pending'), eq(trips.status, 'in progress'))))
+    .where(
+      and(
+        eq(drivers.available, true),
+        isNull(trips.id)
+      )
+    );
+
+  if (tripId) {
+    const driversThatCanceled = await db.select()
+      .from(canceledPendingTrips)
+      .where(eq(canceledPendingTrips.tripId, tripId));
+
+    availableDrivers = availableDrivers.filter(
+      (driver) => !driversThatCanceled.find((canceled) => canceled.driverId === driver.id));
+  }
+
+  if (availableDrivers.length === 0) {
+    return null;
+  }
+
+  const driver = availableDrivers[Math.floor(Math.random() * availableDrivers.length)]!.id;
+  return driver;
+}
 
 const requestTripInput = z.object({
   riderId: z.number(),
@@ -55,13 +85,22 @@ export const tripRouter = createTRPCRouter({
   driverCancelRide: publicProcedure
     .input(z.object({ tripId: z.number() }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.update(trips).set({ status: 'canceled by driver' }).where(eq(trips.id, input.tripId));
+      const trip = await ctx.db.query.trips.findFirst({ where: eq(trips.id, input.tripId )});
+      await ctx.db.insert(canceledPendingTrips).values({ tripId: input.tripId, driverId: trip?.driverId })
+
+      const newDriverID = await getDriverForTrip(ctx.db, input.tripId);
+
+      if (newDriverID) {
+        await ctx.db.update(trips).set({ driverId: newDriverID }).where(eq(trips.id, input.tripId));
+      } else {
+        await ctx.db.update(trips).set({ driverId: null, status: 'no driver available' }).where(eq(trips.id, input.tripId));
+      }
     }),
 
   driverCompleteRide: publicProcedure
     .input(z.object({ tripId: z.number() }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.update(trips).set({ status: 'completed' }).where(eq(trips.id, input.tripId));
+      await ctx.db.update(trips).set({ status: 'complete' }).where(eq(trips.id, input.tripId));
     }),
 
   riderCancelRide: publicProcedure
@@ -81,22 +120,11 @@ export const tripRouter = createTRPCRouter({
         throw new TRPCError({ code: 'BAD_REQUEST', message: "Rider not found" });
       }
 
-      const availableDrivers = await ctx.db.select()
-        .from(drivers)
-        .leftJoin(trips, and(eq(drivers.id, trips.driverId), or(eq(trips.status, 'pending'), eq(trips.status, 'in progress'))))
-        .where(
-          and(
-            eq(drivers.available, true),
-            isNull(trips.id)
-          )
-        );
-
-      if (availableDrivers.length === 0) {
-        throw new TRPCError({ code: 'BAD_REQUEST', message: "No Driver is available, try again later..." });
+      const newDriverID = await getDriverForTrip(ctx.db, null);
+      if (newDriverID) {
+        await ctx.db.insert(trips).values({ ...input, driverId: newDriverID });
+      } else {
+        await ctx.db.insert(trips).values({ ...input, status: 'no driver available', driverId: null });
       }
-
-      const driver = availableDrivers[Math.floor(Math.random() * availableDrivers.length)]?.driver;
-      
-      await ctx.db.insert(trips).values({ driverId: driver?.id, ...input });
     }),
 });
